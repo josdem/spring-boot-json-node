0e5a5932b24d6aa87deb7bdcde032c82f996e0d9
josdem <joseluis.delacruz@gmail.com>
2019-03-09 09:47:30 -0500

    Adding Event model

---
+++ src/main/java/com/jos/dem/springboot/node/object/model/Event.java
@@ -0,0 +1,17 @@
+package com.jos.dem.springboot.node.object;
+
+import java.time.OffsetDateTime;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Event {
+    private String batchId;
+    private String eventType;
+    private String publisher;
+    private String publisherVersion;
+    private OffsetDateTime publishedAt;
+    private Message[] messages;
+}
