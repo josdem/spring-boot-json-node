0a8d8db1511768a5ae84c3672e274e63f43f0d15
josdem <joseluis.delacruz@gmail.com>
2019-03-09 11:02:04 -0500

    Adding logger to the test cases

--- src/test/java/com/jos/dem/springboot/node/object/JsonNodeServiceTest.java
+++ src/test/java/com/jos/dem/springboot/node/object/JsonNodeServiceTest.java
@@ -3,10 +3,13 @@ package com.jos.dem.springboot.node.object;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Date;
 import java.time.OffsetDateTime;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.io.File;
@@ -21,18 +24,31 @@ import com.jos.dem.springboot.node.object.model.Event;
 import com.jos.dem.springboot.node.object.model.Message;
 import com.jos.dem.springboot.node.object.service.JsonNodeReaderService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class JsonNodeServiceTest {
 
+  private Logger log = LoggerFactory.getLogger(this.getClass());
+
   @Autowired
   private JsonNodeReaderService service;
+
+  private Event event;
+
+  @BeforeEach
+  void init() throws Exception {
+    log.info("Getting Event from ClockIn json file");
+    File jsonFile = new File("src/main/resources/ClockIn.json");
+    event = service.read(jsonFile);
+  }
 
   @Test
   @DisplayName("Validate Event values from ClockIn Json file")
   void shouldGetEventFromClockInFile() throws Exception {
-    File jsonFile = new File("src/main/resources/ClockIn.json");
-    Event event = service.read(jsonFile);
+    log.info("Running: Validate Event values from ClockIn Json file at " + new Date());
 
     assertAll("event",
       () -> assertEquals("fbe07c89-ffa7-4c86-9832-5f75cf765737", event.getBatchId(), "Should get batch id"),
@@ -43,10 +59,9 @@ public class JsonNodeServiceTest {
   }
 
   @Test
-  @DisplayName("Validate Message values from ClockIn Json file")
-  void shouldGetMessageFromClockInFile() throws Exception {
-    File jsonFile = new File("src/main/resources/ClockIn.json");
-    Event event = service.read(jsonFile);
+  @DisplayName("Validate Message values from Event")
+  void shouldGetMessageFromEvent() throws Exception {
+    log.info("Running: Validate Message values from event at " + new Date());
 
     Message[] messages = event.getMessages();
     Message message = messages[0];
@@ -59,5 +74,9 @@ public class JsonNodeServiceTest {
 
   }
 
+  @AfterEach
+  void finish() throws Exception {
+    log.info("Test execution finished");
+  }
 
 }
