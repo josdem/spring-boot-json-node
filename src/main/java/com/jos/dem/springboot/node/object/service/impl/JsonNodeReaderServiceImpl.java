d0da53c34de3cb0d461e7aa2836565d9c3c02a5d
josdem <joseluis.delacruz@gmail.com>
2019-03-09 09:15:22 -0500

    Adding Json node reader implementation

--- src/main/java/com/jos/dem/springboot/node/object/service/impl/JsonNodeReaderServiceImpl.java
+++ src/main/java/com/jos/dem/springboot/node/object/service/impl/JsonNodeReaderServiceImpl.java
@@ -1,10 +1,22 @@
 package com.jos.dem.springboot.node.object.service.impl;
 
+import java.io.InputStream;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import org.springframework.stereotype.Service;
+
 import com.jos.dem.springboot.node.object.service.JsonNodeReaderService;
 
+@Service
 public class JsonNodeReaderServiceImpl implements JsonNodeReaderService {
 
+  private ObjectMapper mapper = new ObjectMapper();
+
   public JsonNode read(InputStream inputStream){
+    return  mapper.readTree(exampleInput);
   }
 
 }
