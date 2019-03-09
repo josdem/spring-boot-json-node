4b26f63000ca4d3d2e539d32e5361a22769deb34
josdem <joseluis.delacruz@gmail.com>
2019-03-09 08:31:33 -0500

    Adding Json node service

---
+++ src/main/java/com/jos/dem/springboot/node/object/service/JsonNodeReaderService.java
@@ -0,0 +1,7 @@
+package com.jos.dem.springboot.node.object.service;
+
+public interface JsonNodeReaderService {
+
+  JsonNode read(InputStream inputStream);
+
+}
---
+++ src/main/java/com/jos/dem/springboot/node/object/service/impl/JsonNodeReaderServiceImpl.java
@@ -0,0 +1,10 @@
+package com.jos.dem.springboot.node.object.service.impl;
+
+import com.jos.dem.springboot.node.object.service.JsonNodeReaderService;
+
+public class JsonNodeReaderServiceImpl implements JsonNodeReaderService {
+
+  public JsonNode read(InputStream inputStream){
+  }
+
+}
