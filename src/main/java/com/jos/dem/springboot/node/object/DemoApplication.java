0084adcfacf3f7cca947db91726d7360765940f7
josdem <joseluis.delacruz@gmail.com>
2019-03-09 08:10:37 -0500

    Starting new Spring Boot Node Object Project :)

---
+++ src/main/java/com/jos/dem/springboot/node/object/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.jos.dem.springboot.node.object;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodeObjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NodeObjectApplication.class, args);
+	}
+
+}
