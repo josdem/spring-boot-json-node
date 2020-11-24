a969d7c25708657f4cf81a49d1054903b0c7cb5a
josdem <joseluis.delacruz@gmail.com>
2020-11-24 14:30:59 -0600

    Deleting Travis file

--- src/main/java/com/jos/dem/springboot/json/node/service/impl/UnmarshallerServiceImpl.java
+++ src/main/java/com/jos/dem/springboot/json/node/service/impl/UnmarshallerServiceImpl.java
@@ -1,35 +1,32 @@
 package com.jos.dem.springboot.json.node.service.impl;
 
-import java.io.File;
-import java.io.InputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-
-import javax.annotation.PostConstruct;
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
-import org.springframework.stereotype.Service;
-
 import com.jos.dem.springboot.json.node.model.Event;
 import com.jos.dem.springboot.json.node.service.UnmarshallerService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Service
 public class UnmarshallerServiceImpl implements UnmarshallerService {
 
-  private ObjectMapper mapper = new ObjectMapper();
+    private ObjectMapper mapper = new ObjectMapper();
 
-  @PostConstruct
-	public void setup() {
-    mapper.registerModule(new JavaTimeModule());
-	}
+    @PostConstruct
+    public void setup() {
+        mapper.registerModule(new JavaTimeModule());
+    }
 
-  public Event read(File jsonFile) throws IOException {
-    InputStream inputStream = new FileInputStream(jsonFile);
-    JsonNode jsonNode = mapper.readTree(inputStream);
-    return mapper.treeToValue(jsonNode, Event.class);
-  }
+    public Event read(File jsonFile) throws IOException {
+        InputStream inputStream = new FileInputStream(jsonFile);
+        JsonNode jsonNode = mapper.readTree(inputStream);
+        return mapper.treeToValue(jsonNode, Event.class);
+    }
 
 }
--- src/test/java/com/jos/dem/springboot/json/node/UnmarshallerServiceTest.java
+++ src/test/java/com/jos/dem/springboot/json/node/UnmarshallerServiceTest.java
@@ -1,101 +1,91 @@
 package com.jos.dem.springboot.json.node;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.Date;
-import java.time.OffsetDateTime;
-
 import com.fasterxml.jackson.databind.JsonNode;
-
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
-
-import java.io.File;
-import java.io.InputStream;
-import java.io.FileInputStream;
-
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.jos.dem.springboot.json.node.model.Event;
 import com.jos.dem.springboot.json.node.model.Message;
 import com.jos.dem.springboot.json.node.service.UnmarshallerService;
-
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@ExtendWith(SpringExtension.class)
+import java.io.File;
+import java.time.OffsetDateTime;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
-public class UnmarshallerServiceTest {
+class UnmarshallerServiceTest {
 
-  private Logger log = LoggerFactory.getLogger(this.getClass());
+    private Logger log = LoggerFactory.getLogger(this.getClass());
 
-  @Autowired
-  private UnmarshallerService service;
+    @Autowired
+    private UnmarshallerService service;
 
-  private Event event;
-  private Message message;
-  private Message[] messages;
+    private Event event;
+    private Message message;
+    private Message[] messages;
 
-  @BeforeEach
-  void init() throws Exception {
-    log.info("Getting Event from ClockIn json file");
-    File jsonFile = new File("src/main/resources/ClockIn.json");
-    event = service.read(jsonFile);
-    Message[] messages = event.getMessages();
-    message = messages[0];
-  }
+    @BeforeEach
+    void init() throws Exception {
+        log.info("Getting Event from ClockIn json file");
+        File jsonFile = new File("src/main/resources/ClockIn.json");
+        event = service.read(jsonFile);
+        Message[] messages = event.getMessages();
+        message = messages[0];
+    }
 
-  @Test
-  @DisplayName("Validate Event values from ClockIn Json file")
-  void shouldGetEventFromClockInFile() throws Exception {
-    log.info("Running: Validate Event values from ClockIn Json file at {}", new Date());
+    @Test
+    @DisplayName("Validate Event values from ClockIn Json file")
+    void shouldGetEventFromClockInFile() throws Exception {
+        log.info("Running: Validate Event values from ClockIn Json file at {}", new Date());
 
-    assertAll("event",
-      () -> assertEquals("fbe07c89-ffa7-4c86-9832-5f75cf765737", event.getBatchId(), "Should get batch id"),
-      () -> assertEquals("EmployeeClockIn", event.getEventType(), "Should get event type"),
-      () -> assertEquals(OffsetDateTime.parse("2019-03-09T12:36:43Z"), event.getPublishedAt(), "Should get published at time")
-    );
+        assertAll("event",
+                () -> assertEquals("fbe07c89-ffa7-4c86-9832-5f75cf765737", event.getBatchId(), "Should get batch id"),
+                () -> assertEquals("EmployeeClockIn", event.getEventType(), "Should get event type"),
+                () -> assertEquals(OffsetDateTime.parse("2019-03-09T12:36:43Z"), event.getPublishedAt(), "Should get published at time")
+        );
 
-  }
+    }
 
-  @Test
-  @DisplayName("Validate Message values from Event")
-  void shouldGetMessageFromEvent() throws Exception {
-    log.info("Running: Validate Message values from event at {}", new Date());
+    @Test
+    @DisplayName("Validate Message values from Event")
+    void shouldGetMessageFromEvent() throws Exception {
+        log.info("Running: Validate Message values from event at {}", new Date());
 
-    assertAll("message",
-      () -> assertEquals("4aeaa175-e46d-42eb-83d3-cd02865d4863", message.getMessageId(), "Should get message id"),
-      () -> assertEquals(OffsetDateTime.parse("2019-03-09T12:36:43Z"), event.getPublishedAt(), "Should get published at time")
-    );
+        assertAll("message",
+                () -> assertEquals("4aeaa175-e46d-42eb-83d3-cd02865d4863", message.getMessageId(), "Should get message id"),
+                () -> assertEquals(OffsetDateTime.parse("2019-03-09T12:36:43Z"), event.getPublishedAt(), "Should get published at time")
+        );
 
-  }
+    }
 
-  @Test
-  @DisplayName("Validate Data values from message")
-  void shouldGetDataFromMessage() throws Exception {
-    log.info("Running: Validate Data values from message at {}", new Date());
+    @Test
+    @DisplayName("Validate Data values from message")
+    void shouldGetDataFromMessage() throws Exception {
+        log.info("Running: Validate Data values from message at {}", new Date());
 
-    JsonNode data = message.getData();
-    assertAll("data",
-      () -> assertEquals("Jose", data.get("firstname").textValue(), "Should get Firstname"),
-      () -> assertEquals("Morales", data.get("lastname").textValue(), "Should get Lastname"),
-      () -> assertEquals("josdem", data.get("nickname").textValue(), "Should get Nickname"),
-      () -> assertEquals(1196, data.get("employeeNumber").intValue(), "Should get Employee Number"),
-      () -> assertEquals("joseluis.delacruz@gmail.com", data.get("email").textValue(), "Should get Email"),
-      () -> assertEquals("2019-03-09T07:36:43-05:00", data.get("clockInDateTime").textValue(), "Should get clockIn time")
-    );
+        JsonNode data = message.getData();
+        assertAll("data",
+                () -> assertEquals("Jose", data.get("firstname").textValue(), "Should get Firstname"),
+                () -> assertEquals("Morales", data.get("lastname").textValue(), "Should get Lastname"),
+                () -> assertEquals("josdem", data.get("nickname").textValue(), "Should get Nickname"),
+                () -> assertEquals(1196, data.get("employeeNumber").intValue(), "Should get Employee Number"),
+                () -> assertEquals("joseluis.delacruz@gmail.com", data.get("email").textValue(), "Should get Email"),
+                () -> assertEquals("2019-03-09T07:36:43-05:00", data.get("clockInDateTime").textValue(), "Should get clockIn time")
+        );
 
-  }
+    }
 
-  @AfterEach
-  void finish() throws Exception {
-    log.info("Test execution finished");
-  }
+    @AfterEach
+    void finish() throws Exception {
+        log.info("Test execution finished");
+    }
 
 }
